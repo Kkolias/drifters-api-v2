a10f3ed647ab0a79348d7763045732a0c366e4ce
Elias Koivuporras <eppu2002@gmail.com>
2024-01-22 19:57:47 +0200

    drift-season controller and service

---
+++ src/drift-season/drfit-season.controller.ts
@@ -0,0 +1,102 @@
+import { Request, Response } from "express";
+import driftSeasonService from "./drift-season.service";
+
+export class DriftSeasonController {
+  async createDriftSeason(req: Request, res: Response) {
+    try {
+      const output = await driftSeasonService.handleCreateDriftSeason(req);
+
+      res.status(201).json(output);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to create drift season" });
+    }
+  }
+
+  async getAll(req: Request, res: Response) {
+    try {
+      const qualifyings = await driftSeasonService.findAll(req);
+      res.status(200).json(qualifyings);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to retrieve drift seasons" });
+    }
+  }
+
+  async getById(req: Request, res: Response) {
+    try {
+      const id = req?.query?.id?.toString() || "";
+      const qualifying = await driftSeasonService.findById(id);
+      res.status(200).json(qualifying);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to retrieve drift season" });
+    }
+  }
+
+  async addDriverToDriftSeason(req: Request, res: Response) {
+    try {
+      const { success, error } =
+        await driftSeasonService.handleAddDriverToDriftSeason(req);
+
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json(success);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to update drift season data" });
+    }
+  }
+
+  async addManyDriversToDriftSeason(req: Request, res: Response) {
+    try {
+      const { success, error } =
+        await driftSeasonService.handleAddManyDriversToDriftSeason(req);
+
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json(success);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to update drift season data" });
+    }
+  }
+
+  async addEventToDriftSeason(req: Request, res: Response) {
+    try {
+      const { success, error } =
+        await driftSeasonService.handleAddEventToDriftSeason(req);
+
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json(success);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to update drift season data" });
+    }
+  }
+
+  async addLeaderboardToDriftSeason(req: Request, res: Response) {
+    try {
+      const { success, error } =
+        await driftSeasonService.handleAddLeaderboardToDriftSeason(req);
+
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json(success);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to update drift season data" });
+    }
+  }
+}
+
+export default new DriftSeasonController();
---
+++ src/drift-season/drift-season.service.ts
@@ -0,0 +1,160 @@
+import DriftSeason, { IDriftSeason } from "../Schema/drift/DriftSeason";
+import { DriftSerie } from "../Schema/drift/Leaderboard";
+import driftEventService from "../drift-event/drift-event.service";
+import driverService from "../driver/driver.service";
+import leaderboardService from "../leaderboard/leaderboard.service";
+import { isAdmin } from "../user/utils/isAdmin";
+import { Request } from "express";
+
+export class DriftSeasonService {
+  async findAll(req: Request): Promise<IDriftSeason[]> {
+    const isUserAdmin = await isAdmin(req);
+    if (!isUserAdmin) return [];
+    return await DriftSeason.find().populate("driftEvents").populate('leaderboard').exec();
+  }
+
+  async findById(id: string): Promise<IDriftSeason | null> {
+    return await DriftSeason.findById(id).populate("driftEvents");
+  }
+
+  async createDriftSeason({
+    serie,
+    year,
+  }: {
+    serie: DriftSerie;
+    year: number;
+  }): Promise<IDriftSeason> {
+    const driftSeason = await DriftSeason.create({ serie, year });
+    return driftSeason;
+  }
+
+  async handleCreateDriftSeason(req: Request): Promise<IDriftSeason | null> {
+    const { year, serie } = req.body;
+
+    if (!(await isAdmin(req))) {
+      return null;
+    }
+
+    return await this.createDriftSeason({ serie, year });
+  }
+
+  async addEventToDriftSeason(
+    eventId: string,
+    seasonId: string
+  ): Promise<IDriftSeason | null> {
+    const [driftSeason, driftEvent] = await Promise.all([
+      this.findById(seasonId),
+      driftEventService.findById(eventId),
+    ]);
+
+    if (!driftEvent || !driftSeason) return null;
+
+    driftSeason.driftEvents.push(driftEvent);
+    driftSeason?.save();
+
+    return driftSeason;
+  }
+
+  async handleAddEventToDriftSeason(
+    req: Request
+  ): Promise<{ error: string; success: IDriftSeason | null }> {
+    const { eventId, seasonId } = req.body;
+
+    const driftEvent = await this.addEventToDriftSeason(eventId, seasonId);
+
+    if (!driftEvent) return { error: "updating season failed", success: null };
+    return { error: "", success: driftEvent };
+  }
+
+  async addDriverToDriftSeason(
+    driverId: string,
+    seasonId: string
+  ): Promise<IDriftSeason | null> {
+    const [driftSeason, driver] = await Promise.all([
+      this.findById(seasonId),
+      driverService.findById(driverId),
+    ]);
+
+    if (!driver || !driftSeason) return null;
+
+    driftSeason.driversOfSeason.push(driver);
+    driftSeason?.save();
+
+    return driftSeason;
+  }
+
+  async handleAddDriverToDriftSeason(
+    req: Request
+  ): Promise<{ error: string; success: IDriftSeason | null }> {
+    const { driverId, seasonId } = req.body;
+
+    const driftEvent = await this.addDriverToDriftSeason(driverId, seasonId);
+
+    if (!driftEvent) return { error: "updating season failed", success: null };
+    return { error: "", success: driftEvent };
+  }
+
+  async addManyDriversToDriftSeason(
+    driverIdList: string[],
+    seasonId: string
+  ): Promise<IDriftSeason | null> {
+    const [driftSeason, drivers] = await Promise.all([
+      this.findById(seasonId),
+      driverService.findByIdList(driverIdList),
+    ]);
+
+    if (!drivers || !driftSeason) return null;
+
+    driftSeason.driversOfSeason.push(...drivers);
+    driftSeason?.save();
+
+    return driftSeason;
+  }
+
+  async handleAddManyDriversToDriftSeason(
+    req: Request
+  ): Promise<{ error: string; success: IDriftSeason | null }> {
+    const { driverIdList, seasonId } = req.body;
+
+    const driftEvent = await this.addManyDriversToDriftSeason(
+      driverIdList,
+      seasonId
+    );
+
+    if (!driftEvent) return { error: "updating season failed", success: null };
+    return { error: "", success: driftEvent };
+  }
+
+  async addLeaderboardToDriftSeason(
+    leaderboardId: string,
+    seasonId: string
+  ): Promise<IDriftSeason | null> {
+    const [driftSeason, leaderboard] = await Promise.all([
+      this.findById(seasonId),
+      leaderboardService.findById(leaderboardId),
+    ]);
+
+    if (!leaderboard || !driftSeason) return null;
+
+    driftSeason.leaderboard = leaderboard;
+    driftSeason?.save();
+
+    return driftSeason;
+  }
+
+  async handleAddLeaderboardToDriftSeason(
+    req: Request
+  ): Promise<{ error: string; success: IDriftSeason | null }> {
+    const { leaderboardId, seasonId } = req.body;
+
+    const driftEvent = await this.addLeaderboardToDriftSeason(
+      leaderboardId,
+      seasonId
+    );
+
+    if (!driftEvent) return { error: "updating season failed", success: null };
+    return { error: "", success: driftEvent };
+  }
+}
+
+export default new DriftSeasonService();
