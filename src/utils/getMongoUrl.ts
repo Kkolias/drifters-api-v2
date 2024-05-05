20eefafdc0c91c161977094d5f082f12632f5cc2
Elias Koivuporras <eppu2002@gmail.com>
2024-05-05 16:26:15 +0300

    getMongoUrl change

--- src/utils/getMongoUrl.ts
+++ src/utils/getMongoUrl.ts
@@ -2,8 +2,8 @@ import { isProduction } from "./isProduction";
 
 export function getMongoUrl(): string {
     if(isProduction()) {
-        return `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@driftdataancluster0.msh5f0i.mongodb.net/`
+        return `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/`
     }
-    return `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@driftdataancluster0.msh5f0i.mongodb.net/`
+    return `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/`
     // return "mongodb://admin:password@localhost:27018/?authMechanism=DEFAULT"
 }
